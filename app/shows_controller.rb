b0fad9c3e57ebb05a5947bb58b3c54b3ff0e7a95
Ramsey Chapin <ramsey@ramseychapin.com>
2020-01-22 04:15:04 +0000

    Automatically backed up by Learn

--- app/shows_controller.rb
+++ app/shows_controller.rb
@@ -1,31 +1,31 @@
-
-
-
-def self.highest_rating
-  Show.maximum(:rating)
-end
-
-
-def most_popular_show
-
-end
-
-def lowest_rating
-
-end
-
-def least_popular_show
-
-end
-
-def ratings_sum
-
-end
-
-def popular_shows
-
-end
-
-def shows_by_alphabetical_order
-
-end
+#
+#
+#
+# def self.highest_rating
+#   Show.maximum(:rating)
+# end
+#
+#
+# def most_popular_show
+#
+# end
+#
+# def lowest_rating
+#
+# end
+#
+# def least_popular_show
+#
+# end
+#
+# def ratings_sum
+#
+# end
+#
+# def popular_shows
+#
+# end
+#
+# def shows_by_alphabetical_order
+#
+# end
