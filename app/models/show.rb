bdf20de53e00e1bb4ea388f4b8b60179a5cf699f
Ramsey Chapin <ramsey@ramseychapin.com>
2020-01-22 04:36:50 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,7 +4,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    Show.where("rating = ?", self.lowest_rating)
+    Show.where("rating = ?", self.highest_rating)
   end
 
   def self.lowest_rating
