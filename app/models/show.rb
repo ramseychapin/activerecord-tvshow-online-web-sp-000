b8a48a59f539f383bc8a4a7d44621b386d6f0ff5
Ramsey Chapin <ramsey@ramseychapin.com>
2020-01-22 04:21:18 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,4 +2,30 @@ class Show < ActiveRecord::Base
   def self.highest_rating
     Show.maximum(:rating)
   end
+
+  def self.most_popular_show
+
+  end
+
+  def lowest_rating
+
+  end
+
+  def least_popular_show
+
+  end
+
+  def ratings_sum
+
+  end
+
+  def popular_shows
+
+  end
+
+  def shows_by_alphabetical_order
+
+  end
+
+
 end
