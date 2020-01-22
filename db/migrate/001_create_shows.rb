01a2ff0f1671d30e19cc0345555da8d6101a5b15
Ramsey Chapin <ramsey@ramseychapin.com>
2020-01-22 01:58:12 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,10 +1,10 @@
 class CreateShows < ActiveRecord::Migration[5.2]
   def change
     create_table :shows do |t|
-      name
-      network
-      day
-      rating
+      t.string :name
+      t.string :network
+      t.string :day
+      t.integer :rating
     end
   end
 end
