fbcc6c455bb7293bcd89f3932473dbc4d853ea21
Ramsey Chapin <ramsey@ramseychapin.com>
2020-01-22 04:28:57 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,7 +4,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    
+
   end
 
   def self.lowest_rating
@@ -20,7 +20,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.popular_shows
-
+    
   end
 
   def self.shows_by_alphabetical_order
